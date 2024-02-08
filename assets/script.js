1514045fe548d976267edd352c31425f15f02a27
roellongcop <longcoproel@gmail.com>
2024-02-08 18:13:52 +0800

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var closeToggler = document.querySelector('.close-icon-container button');
+    var menu = document.getElementById('navbarSupportedContent');
+
+    // Function to explicitly open or close the menu
+    function openMenu() {
+        menu.classList.add('show-menu');
+    }
+
+    function closeMenu() {
+        menu.classList.remove('show-menu');
+    }
+
+    closeToggler.addEventListener('click', function(event) {
+        event.stopPropagation();
+        closeMenu();
+    });
+
+    // Toggle the menu when the navbar toggler is clicked
+    navbarToggler.addEventListener('click', function(event) {
+        // Prevent the click from being propagated to the document
+        event.stopPropagation();
+
+        if (menu.classList.contains('show-menu')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+
+    // Close the menu when clicking outside of it
+    document.addEventListener('click', function(event) {
+        if (!menu.contains(event.target) && !navbarToggler.contains(event.target)) {
+            closeMenu();
+        }
+    });
+});
