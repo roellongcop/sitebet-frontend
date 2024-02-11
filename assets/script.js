530c29a5103381a079d077e336c1dcfb28922923
roellongcop <longcoproel@gmail.com>
2024-02-11 14:44:04 +0800

    c

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,85 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var navbarToggler = document.querySelector('.navbar-toggler');
-    var closeToggler = document.querySelector('.close-icon-container button');
-    var menu = document.getElementById('navbarSupportedContent');
+  // Define jQuery selectors for the elements
+  var $navbarToggler = $('.navbar-toggler');
+  var $closeToggler = $('.close-icon-container button');
+  var $menu = $('#navbarSupportedContent');
 
-    // Function to explicitly open or close the menu
-    function openMenu() {
-        menu.classList.add('show-menu');
+  // Function to explicitly open or close the menu
+  function openMenu() {
+    $menu.addClass('show-menu');
+  }
+
+  function closeMenu() {
+    $menu.removeClass('show-menu');
+  }
+
+  // Handle click on the close toggler
+  $closeToggler.click(function(event) {
+    event.stopPropagation();
+    closeMenu();
+  });
+  // Toggle the menu when the navbar toggler is clicked
+  $navbarToggler.click(function(event) {
+    event.stopPropagation(); // Prevent the click from being propagated to the document
+
+    if ($menu.hasClass('show-menu')) {
+      closeMenu();
+    } else {
+      openMenu();
     }
+  });
 
-    function closeMenu() {
-        menu.classList.remove('show-menu');
+  // Close the menu when clicking outside of it
+  $(document).click(function(event) {
+    // Check if the target of the click is not the menu or navbar toggler
+    if (!$menu.is(event.target) && !$menu.has(event.target).length && !$navbarToggler.is(event.target) && !$navbarToggler.has(event.target).length) {
+      closeMenu();
     }
+  });
 
-    closeToggler.addEventListener('click', function(event) {
-        event.stopPropagation();
-        closeMenu();
-    });
+  $('.img-container').each(function() {
+    var imgSrc = $(this).attr('data-src');
+    if (imgSrc) {
+      $(this).css('background-image', 'url(' + imgSrc + ')');
+    }
+  });
 
-    // Toggle the menu when the navbar toggler is clicked
-    navbarToggler.addEventListener('click', function(event) {
-        // Prevent the click from being propagated to the document
-        event.stopPropagation();
+  $('.language-dropdown').on('mouseover', function() {
+    $(this).find('.dropdown-menu').show();
+  }).on('mouseleave', function() {
+    $(this).find('.dropdown-menu').hide();
+  })
 
-        if (menu.classList.contains('show-menu')) {
-            closeMenu();
-        } else {
-            openMenu();
-        }
-    });
+  $('.language-dropdown .dropdown-menu .dropdown-item').on('click', function() {
+    const value = $(this).data('value');
+    const src = $(this).find('img').attr('src');
+    $(this).closest('.dropdown-menu').hide();
+    $('.selected-language').text(value);
+    $('.btn-language img').attr('src', src);
+    $(this).closest('.dropdown-menu').find('.dropdown-item').removeClass('active');
+    $(this).addClass('active');
+  });
 
-    // Close the menu when clicking outside of it
-    document.addEventListener('click', function(event) {
-        if (!menu.contains(event.target) && !navbarToggler.contains(event.target)) {
-            closeMenu();
-        }
-    });
+  $('main .tab-container .tab-item').on('click', function(e) {
+    e.preventDefault();
+    $(this).closest('.tab-main-container').find('.tab-item').removeClass('active')
+    $(this).addClass('active')
+  });
 
-    var imgContainers = document.querySelectorAll('.img-container');
+  $('.deposit-tab .payment-option-container button.btn-block').on('click', function() {
+    $('.deposit-tab .payment-option-container button.btn-block').removeClass('active');
+    $(this).addClass('active')
+  })
 
-    imgContainers.forEach(function(container) {
-        var imgSrc = container.getAttribute('data-src');
-        if (imgSrc) {
-            container.style.backgroundImage = 'url(' + imgSrc + ')';
-        }
-    });
+  $('.search-container .dropdown-menu li').on('click', function(e) {
+    e.preventDefault();
+    const value = $(this).find('a').data('value');
+    $(this).closest('.dropdown').find('.dropdown-toggle').text(`${value}`)
+  })
 
+  $('.country-code-container .dropdown-menu li').on('click', function(e) {
+    e.preventDefault();
+    const value = $(this).find('a').data('value');
+    $(this).closest('.dropdown').find('.dropdown-toggle').text(`${value}`)
+  })
 });
